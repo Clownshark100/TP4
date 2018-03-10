31de3b4582c5a102bf37317904327f4bb636cf6d
Daniel Nahum <danah@polymtl.ca>
2018-03-10 16:59:35 -0500

    reponse au questions modifier

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 *
 * Questions:
 * 1)  1
-* 2)  puisque la methode de la classe de base n'est pas virtual.
-* 3) d'abord il faudra rendre la methode de la classe A virtual. deuxieme solution : static_cast???
+* 2)  puisque la methode de la classe parent n'est pas virtual.
+* 3) De un, on peut enlever le virtual de la classe B ce qui fera en sorte que l'objet de classe a appelera la fonction de son parent B.
+* De deux, on peut appeler la fonction de maniere explicite : b ->B::f();
 * 4) Usager a une methode virtuel pure, ce qui fait que c'est une classe abstraite.
 * 5) On peut utiliser un dynamic_cast<produitSolde> et verifier si le produit est un produit solde.
 *******************************************/
